55121789ca6392879383a1452b69ebfb6e650a50
Venkateswara VP <venkateswara.vp@gmail.com>
2020-05-05 12:33:20 -0400

    The @Prototype annotation is a synonym for @Bean because the default scope is prototype.

--- src/main/java/io/vpv/saml/BeanFactory.java
+++ src/main/java/io/vpv/saml/BeanFactory.java
@@ -1,13 +1,14 @@
 package io.vpv.saml;
 
-import io.micronaut.context.annotation.Bean;
 import io.micronaut.context.annotation.Factory;
 import io.vpv.saml.metadata.service.MetaDataParser;
 import io.vpv.saml.metadata.service.MetaDataParserImpl;
 
+import javax.inject.Singleton;
+
 @Factory
 public class BeanFactory {
-    @Bean
+    @Singleton
     public MetaDataParser getMetaDataParser() {
         return new MetaDataParserImpl();
     }
